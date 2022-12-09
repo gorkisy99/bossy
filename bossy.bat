d65fe0d43ca66973737b6c62b080ce00efbc4502
gorkisy99 <115589852+gorkisy99@users.noreply.github.com>
2022-12-09 14:51:45 +0100

    Update bossy.bat

--- bossy.bat
+++ bossy.bat
@@ -4,3 +4,12 @@ $ git clone https://github.com/YOUR-USERNAME/YOUR-REPOSITORY
 > remote: Compressing objects: 100% (8/8), done.
 > remove: Total 10 (delta 1), reused 10 (delta 1)
 > Unpacking objects: 100% (10/10), done.
+<title bossy>
+<base href="messi">
+<a href="http://ww.youtube.com"></a>
+<del><script></script></del>
+<picture>
+    <source media="(dd)" srcset="">
+    <img src="" alt="">
+</picture>
+<em><embed src="Unpacking" type="add"></em></em><s><a href="mailto:"></a></s>
